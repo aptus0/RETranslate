9a5b79c7aeddc1b53ef46af65bd081c4c9830499
aptus0 <temasre@tutanota.com>
2025-10-20 17:13:47 +0300

    Initial Commit

---
+++ RETranslate/RETranslateApp.swift
@@ -0,0 +1,32 @@
+//
+//  RETranslateApp.swift
+//  RETranslate
+//
+//  Created by Samet on 20.10.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RETranslateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RETranslateTests/RETranslateTests.swift
@@ -0,0 +1,17 @@
+//
+//  RETranslateTests.swift
+//  RETranslateTests
+//
+//  Created by Samet on 20.10.2025.
+//
+
+import Testing
+@testable import RETranslate
+
+struct RETranslateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
